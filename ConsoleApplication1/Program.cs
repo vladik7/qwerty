692d77b6af401b9a600f66d0cfaa639b32f30054
vldik7 <vladmart1996@mail.ru>
2016-04-13 23:06:11 +0300

    no message

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,48 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApplication1
+﻿namespace ConsoleApplication1
 {
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1400:AccessModifierMustBeDeclared", Justification = "Reviewed. Suppression is OK here.")]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
     class Program
     {
-
-        static void Main(string[] args)
+        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
+        static void Main()
         {
-           var a = Console.ReadLine();
-           var b = Console.ReadLine();
-           var c = Console.ReadLine();
+            var a = Console.ReadLine();
+            var b = Console.ReadLine();
+            var c = Console.ReadLine();
             double a1 = 0;
             double b1 = 0;
             double c1 = 0;
             if (double.TryParse(a, out a1) && double.TryParse(b, out b1) && double.TryParse(c, out c1))
             {
-                double d = b1 * b1 - 4 * a1 * c1;
+
+                var d = b1 * b1 - 4 * a1 * c1;
                 if (d >= 0)
                 {
                     if (d > 0)
                     {
-                        double x1 = (-b1+Math.Pow(d,0.5))/(2*a1);
-                        double x2 = (-b1 - Math.Pow(d, 0.5)) / (2 * a1);
-                        Console.WriteLine("x1 = {0}",x1);
-                        Console.WriteLine("x2 = {0}",x2);
+                        var x1 = (-b1 + Math.Pow(d, 0.5)) / (2 * a1);
+                        var x2 = (-b1 - Math.Pow(d, 0.5)) / (2 * a1);
+                        Console.WriteLine("x1 = {0}", x1);
+                        Console.WriteLine("x2 = {0}", x2);
                     }
                     else
                     {
                         double x1 = (-b1 + Math.Pow(d, 0.5)) / (2 * a1);
-                        Console.WriteLine("x1 = x2 = {0}",x1);
+                        Console.WriteLine("x1 = x2 = {0}", x1);
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Kornei net");
+                    Console.WriteLine("Корней нет");
                 }
             }
             else
             {
-                Console.WriteLine("Vvedi chisla");
+                Console.WriteLine("Введите числа");
             }
         }
     }
